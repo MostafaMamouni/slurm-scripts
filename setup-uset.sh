d5ad75124315e2a7eb62d0176a28d4db8667fda2
MostafaMamouni <32802212+MostafaMamouni@users.noreply.github.com>
2022-03-17 15:13:27 +0100

    Create new file

---
+++ setup-uset.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
+
+if [ $1 == "-h"]; then
+		echo "usage: ./create_users_account username [--dacc defaultaccount cpuhours] [--acc account cpuhours] [--qos qos]"
+		exit 0
+fi
+
+
+if [ $1 ]; then
+	if ! id $1 > /dev/null 2>&1; then
+		echo error: "\"$1\" user doesn't exit"
+		exit 1
+	fi
+	if [[ "$1" == [^\-\-] ]]; then
+		echo the first argument should be the username
+		echo "usage: ./create_users_account username [--dacc defaultaccount cpuhours] [--acc account cpuhours] [--qos qos]"
+		exit 1
+	fi
+	USER="$1"
+fi
+
+shift 1
+
+while [ : ];do
+	if [ "$1" == "--acc" ]; then
+		if [ ! $2 ] || [[ "$2" == [^\-\-] ]]; then
+			echo acc option requires an argument
+			exit 1
+		fi
+		if [ ! $3 ] ; then
+			echo dacc option requires a second argument cpuhours
+			exit 1
+		fi
+		if [[ "$3" =~ [^0-9] ]]; then
+			echo error: cpuhours should be a number
+			exit 1
+		fi
+		ACPUHOURS=$3
+		shift 1
+		ACCOINT=$2
+	elif [ "$1" == "--dacc" ]; then
+		if [ ! $2 ] || [[ "$2" == [^\-\-] ]]; then
+			echo dacc option requires an argument
+			exit 1
+		fi
+		if [ ! $3 ] ; then
+			echo dacc option requires a second argument cpuhours
+			exit 1
+		fi
+		if [[ "$3" =~ [^0-9] ]]; then
+			echo error: cpuhours should be a number
+			exit 1
+		fi
+		DACPUHOURS=$3
+		shift 1
+		DEFAULTACCOUNT="$2-account"
+	elif [ "$1" == "--qos" ]; then
+		if [ ! $2 ] || [[ "$2" == [^\-\-] ]]; then
+			echo qos option requires an argument
+			exit 1
+		fi
+		QOS=$2
+	else
+		break
+	fi
+	shift 2
+done
+
+if [ $ACCOUNT ]; then
+	echo -e "${GREEN}"
+	echo "sacctmgr add account $ACCOUNT"
+	echo -e ${NOCOLOR}
+#	sacctmgr add account $ACCOUNT
+	echo -e ${GREEN}
+	echo "sacctmgr add user $USER account=$ACCOUNT"
+	echo -e ${NOCOLOR}
+#	sacctmgr add user $USER account=$ACCOUNT
+fi
+
+if [ $DEFAULTACCOUNT ]; then
+	echo -e "${GREEN}"
+	echo "sacctmgr add account $DEFAULTACCOUNT"
+	echo -e ${NOCOLOR}
+#	sacctmgr add account $ACCOUNT
+	echo -e ${GREEN}
+	echo "sacctmgr add user $USER defaultaccount=$ACCOUNT"
+	echo -e ${NOCOLOR}
+#	sacctmgr add user $USER defaultaccount=$ACCOUNT
+fi
+if [ $QOS ]; then
+	echo -e ${GREEN}
+	echo "sacctmgr modify user $USER set qos=$QOS"
+	echo -e ${NOCOLOR}
+#	sacctmgr modify user $USER set qos=$4
+fi
+
+if [ $ACPUHOURS ]; then
+	echo -e ${GREEN}
+	echo "sbank deposit -c slurm_cluster -a $ACCOUNT -t $ACPUHOURS"
+	echo -e ${NOCOLOR}
+#	sbank deposit -c slurm_cluster -a $ACCOUNT -t $CPUHOURS
+fi
+
+if [ $DACPUHOURS ]; then
+	echo -e ${GREEN}
+	echo "sbank deposit -c slurm_cluster -a $DEFAULTACCOUNT -t $DACPUHOURS"
+	echo -e ${NOCOLOR}
+#	sbank deposit -c slurm_cluster -a $ACCOUNT -t $CPUHOURS
+fi
